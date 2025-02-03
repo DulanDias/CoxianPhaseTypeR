fc52ffb89b4b495541deb16905d7030fdd8b89ac
Dulan Dias <dulan@echonlabs.com>
2025-02-03 02:40:35 +0000

    remove duplicates before ks

--- R/compute_goodness_of_fit.R
+++ R/compute_goodness_of_fit.R
@@ -8,6 +8,9 @@ compute_goodness_of_fit <- function(fit, data) {
   library(survival)
   library(stats)
 
+  # Remove duplicate survival times for KS test robustness
+  data <- unique(data)
+
   # Compute empirical survival function (Kaplan-Meier)
   km_fit <- survfit(Surv(data) ~ 1)
   km_survival <- stepfun(km_fit$time, c(1, km_fit$surv))
@@ -20,12 +23,20 @@ compute_goodness_of_fit <- function(fit, data) {
     }))
   })
 
-  # Compute RMSE
+  # Compute RMSE with robust handling for survival probabilities
   empirical_survival <- km_survival(t_values)
-  rmse <- sqrt(mean((empirical_survival - model_survival)^2))
+  valid_indices <- !is.na(empirical_survival) & !is.na(model_survival)
 
-  # Compute KS statistic, suppressing warnings for ties
-  ks_stat <- suppressWarnings(ks.test(empirical_survival, model_survival)$statistic)
+  if (sum(valid_indices) == 0) {
+    warning("No valid survival probabilities for RMSE calculation. Returning NA.")
+    rmse <- NA
+  } else {
+    rmse <- sqrt(mean((empirical_survival[valid_indices] - model_survival[valid_indices])^2))
+  }
+
+  # Compute KS statistic, using empirical CDF and interpolated model survival
+  ks_stat <- suppressWarnings(ks.test(empirical_survival[valid_indices],
+                                      model_survival[valid_indices])$statistic)
 
   return(list(RMSE = rmse, KS_statistic = ks_stat))
 }
